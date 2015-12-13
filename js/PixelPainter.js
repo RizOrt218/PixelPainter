0a4c13f04eb70ab8587e7ddac5ef2964b196ac08
RizOrt218 <rizhiafalen@gmail.com>
2015-12-12 20:03:50 -1000

    little update-working on undo button

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -28,74 +28,85 @@
 
   //create function to show default color swatch
 
-window.onload = function(){
+window.onload = function() {
   var newColor;
+  var dragColor;
 
-  var main = document.getElementById(('pixelPainter'));
+  var main = document.getElementById( 'pixelPainter' );
 
   //ELEMENT DIV color
-  var colors = document.createElement('div');
+  var colors = document.createElement( 'div' );
     colors.className = 'colorGrid';
-    main.appendChild(colors);
+    main.appendChild( colors );
 
+  var undoButton = document.createElement( 'button' );//<------------button element
+    undoButton.className = 'undoButton';
+    main.appendChild( undoButton );
 
-  for( var i = 1; i <= 12; i++){
-    var colorRow = document.createElement('div');
+  for( var i = 1; i <= 12; i++ ){
+    var colorRow = document.createElement( 'div' );
     colorRow.id = 'colorRow' + i;
     colorRow.className = 'colorRow';
-    colors.appendChild(colorRow);
+    colors.appendChild( colorRow );
 
-    for( var j = 1; j <= 6; j++){
-      var colorBtn = document.createElement('div');
+    for( var j = 1; j <= 6; j++ ){
+      var colorBtn = document.createElement( 'div' );
       colorBtn.id = 'colorBtn' + j;
       colorBtn.className = 'colorBtn';
-      colorRow.appendChild(colorBtn);
+      colorRow.appendChild( colorBtn );
 
-      colorBtn.addEventListener('click', function(){
+      colorBtn.addEventListener( 'click', function() {
         newColor = this.style.background;
         return newColor;
       });
 
     }
   }
-
-  var dragColor;
+  var stroke = false;
+  var undoArray = [];
 
   //ELEMENT DIV pixelGrid
-  var pixelGrid = document.createElement('div');
+  var pixelGrid = document.createElement( 'div' );
     pixelGrid.className = 'pixelGrid';
-    main.appendChild(pixelGrid);
+    main.appendChild( pixelGrid );
 
-  for( var k = 1; k <= 20; k++){
-    var canvasRow = document.createElement('div');
-    canvasRow.id = 'canvasRow' + i;
-    canvasRow.className = 'canvasRow';
-    pixelGrid.appendChild(canvasRow);
+  for( var k = 1; k <= 50; k++ ){
+    var canvasRow = document.createElement( 'div' );
+    canvasRow.id = 'canvasRow';
+    canvasRow.className = 'canvasRow' + k;
+    pixelGrid.appendChild( canvasRow );
 
-    for( var p = 1; p <= 20; p++){
-      var canPixels = document.createElement('div');
+    for( var p = 1; p <= 50; p++ ){
+      var canPixels = document.createElement( 'div' );
       canPixels.id = 'canPixels' + p;
       canPixels.className = 'canPixels';
-      canvasRow.appendChild(canPixels);
+      canvasRow.appendChild( canPixels );
 
       //when click current color will appear
-      canPixels.addEventListener('click', function(){
+      canPixels.addEventListener( 'click', function() {//<---------click fn
+        var stroke = true;
+          console.log(stroke);//<-----------------------------------stroke
 
         if( dragColor === this.style.background ){
           dragColor = !newColor;
-          console.log(newColor);
           return dragColor;
         }
 
         this.style.background = newColor;
-        this.style.opacity = 1;
         dragColor = newColor;
         return dragColor;
+
       });
 
       //then when dragged, will appear current color
-      canPixels.addEventListener('mouseover', function(){
+      canPixels.addEventListener( 'mouseover', function() {
         this.style.background = dragColor;
+
+        while( stroke === true ){
+          undoArray.push( canPixels );
+          console.log(undoArray);//<---------------------------------push!
+          return undoArray;
+        }
       });
     }
   }
